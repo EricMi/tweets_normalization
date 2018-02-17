76e6284876c50710e50034eefbb262b0229f2de1
Ubuntu <pmi@pmi2.lgcu0yh1n2pu3kljq1u3lg5ppa.ax.internal.cloudapp.net>
2018-02-17 23:44:20 +0000

    Modify file path.

--- utils/download_data.sh
+++ utils/download_data.sh
@@ -1,18 +1,18 @@
-mkdir ../cache
-mkdir ../corpus
-mkdir ../models
+mkdir ./cache
+mkdir ./corpus
+mkdir ./models
 
 # context2vector pre-trained models
-wget http://irsrv2.cs.biu.ac.il/downloads/context2vec/context2vec.mscc.model.package.tar.gz -P ../cache
-wget http://irsrv2.cs.biu.ac.il/downloads/context2vec/context2vec.ukwac.model.package.tar.gz -P ../cache
+wget http://irsrv2.cs.biu.ac.il/downloads/context2vec/context2vec.mscc.model.package.tar.gz -P ./cache
+wget http://irsrv2.cs.biu.ac.il/downloads/context2vec/context2vec.ukwac.model.package.tar.gz -P ./cache
 
 # GloVe word vectors pre-trained on Twitter corpus
-wget http://nlp.stanford.edu/data/glove.twitter.27B.zip -P ../cache
+wget http://nlp.stanford.edu/data/glove.twitter.27B.zip -P ./cache
 
 # Bataclan twitte corpus
-wget https://github.com/edupoux/MVA_2018_SL/blob/master/TD_%233/CorpusBataclan_en.1M.raw.txt.gz -P ../cache
+wget https://github.com/edupoux/MVA_2018_SL/blob/master/TD_%233/CorpusBataclan_en.1M.raw.txt.gz -P ./cache
 
 # Decompress data files
-tar -xzvf cache/context2vec.mscc.model.package.tar.gz -C ../models/c2v.mscc.model
-tar -xzvf cache/context2vec.ukwac.model.package.tar.gz -C ../models/c2v.ukwac.model
-tar -xzvf cache/CorpusBataclan_en.1M.raw.txt.gz -C ../corpus
+tar -xzvf cache/context2vec.mscc.model.package.tar.gz -C ./models/c2v.mscc.model
+tar -xzvf cache/context2vec.ukwac.model.package.tar.gz -C ./models/c2v.ukwac.model
+tar -xzvf cache/CorpusBataclan_en.1M.raw.txt.gz -C ./corpus
