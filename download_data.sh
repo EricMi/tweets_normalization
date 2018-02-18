6ded9fc6f6694ad6ea739080d9c2e7a5444daca1
EricMi <mipengfei1995@gmail.com>
2018-02-18 23:56:30 +0100

    Updaty .gitignore file to include necessary fiels for demo.

--- download_data.sh
+++ download_data.sh
@@ -1,6 +1,4 @@
-mkdir ./cache
-mkdir ./corpus
-mkdir ./models
+mkdir -p ./cache
 
 # clone context2vec repo.
 git clone https://github.com/orenmel/context2vec.git
@@ -13,9 +11,17 @@ wget http://irsrv2.cs.biu.ac.il/downloads/context2vec/context2vec.ukwac.model.pa
 wget http://nlp.stanford.edu/data/glove.twitter.27B.zip -P ./cache
 
 # Bataclan twitte corpus
-wget https://github.com/edupoux/MVA_2018_SL/blob/master/TD_%233/CorpusBataclan_en.1M.raw.txt.gz -P ./cache
+#wget https://github.com/edupoux/MVA_2018_SL/raw/master/TD_%233/CorpusBataclan_en.1M.raw.txt.gz -P ./cache
 
 # Decompress data files
+mkdir ./models/c2v.mscc.model
+mkdir ./models/c2v.ukwac.model
 tar -xzvf cache/context2vec.mscc.model.package.tar.gz -C ./models/c2v.mscc.model
 tar -xzvf cache/context2vec.ukwac.model.package.tar.gz -C ./models/c2v.ukwac.model
-tar -xzvf cache/CorpusBataclan_en.1M.raw.txt.gz -C ./corpus
+#gzip -d ./cache/CorpusBataclan_en.1M.raw.txt.gz
+#mv ./cache/CorpusBataclan_en.1M.raw.txt ./corpus
+
+# Dictionary files
+#wget https://raw.githubusercontent.com/cbaziotis/ekphrasis/master/ekphrasis/dicts/noslang/manager.py -P ./dicts
+#wget https://github.com/cbaziotis/ekphrasis/raw/master/ekphrasis/dicts/noslang/slangdict.pickle -P ./dicts
+#wget https://github.com/dwyl/english-words/raw/master/words_alpha.txt -P ./dicts
